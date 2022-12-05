b0dceccc571569061e60a614f610bea785755bf7
BruLau <brunomlauricella@gmail.com>
2022-12-05 11:15:51 -0300

    avances en el menu

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 
 import { StyleSheet, Text, View, } from 'react-native';
 import Main from './src/components/Main.jsx';
+import Contacto from './src/components/Contacto';
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Menu= createDrawerNavigator()
+
 export default function App() {
   return (
-   <Main ></Main>
-  
+   
+  <NavigationContainer>
+<Menu.Navigator>
+  <Menu.Screen name = "Inicio" component={Main}/>
+  <Menu.Screen name = "Contacto" component={Contacto}/>
+</Menu.Navigator>
+  </NavigationContainer>
   );
 }
 const styles = StyleSheet.create({
--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -3,10 +3,11 @@ import { Text, View, StyleSheet, Image } from "react-native";
 import Constants from "expo-constants"
 import logo from "../../assets/logo.jpg"
 //resizeMode:"contain"
-const Main = () => {
+const Contacto = () => {
     return(
 <View style={ styles.container}>
     <Image style={styles.image} source={logo}></Image>
+    <Text>Contacto</Text>
 </View>
     )
 }
@@ -14,4 +15,4 @@ const styles = StyleSheet.create({
     container:{backgroundColor:"white", alignItems:"center", flex: 1, justifyContent:"flex-start", marginTop:80},
     image: {height:81 , width:368}
 })
-export default Main
+export default Contacto
